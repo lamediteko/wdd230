98ff8f860da2247b2ea8e2fd4f4d39fed0104183
lamediteko <97170714+lamediteko@users.noreply.github.com>
2022-07-02 06:27:54 +0200

    corrections catch up

--- week05/js/bom-scripts.js
+++ week05/js/bom-scripts.js
@@ -1,19 +1,30 @@
-const input = document.getElementById("favchap");
-const button = document.querySelector("button");
-const list = document.querySelector(".list");
 
-button.addEventListener('click', function () {
-    if (input.value) {
-        let newListItem = document.createElement('li');
-        let newDeleteButton = document.createElement('button');
-        newListItem.innerHTML = input.value;
-        newDeleteButton.textContent = "❌";
-        newListItem.appendChild(newDeleteButton);
-        list.appendChild(newListItem);
-        newDeleteButton.addEventListener('click', function() {
-            newListItem.remove();
-        });
-        input.value = "";
-        input.focus();
-        }
-    })
+const ulTag = document.querySelector(".list");
+const inputTag = document.querySelector("#fav-chap");
+const buttonTag = document.querySelector("#submit-button");
+
+buttonTag.addEventListener("click", () => {
+  let inputValue = inputTag.value;
+  if (inputValue.trim() === "") {
+    alert("Empty field not allowed. You must insert a value in order to save it");
+  } else {
+    inputTag.value = "";
+    let liElement = document.createElement("li");
+    let spanElement = document.createElement("span");
+    let buttonElement = document.createElement("button");
+    liElement.appendChild(spanElement);
+    liElement.appendChild(buttonElement);
+    spanElement.textContent = inputValue;
+    buttonElement.textContent = "X";
+    buttonElement.style.color = "red";
+    buttonElement.setAttribute("arial-label", "Remove this book and chapter");
+
+    ulTag.appendChild(liElement);
+
+    buttonElement.addEventListener("click", () => {
+      ulTag.removeChild(liElement);
+      inputTag.focus();
+    });
+  }
+  inputTag.focus();
+});
