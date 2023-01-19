b913fb83a4485e75461efa025ab00b8cd37d9686
lamediteko <97170714+lamediteko@users.noreply.github.com>
2023-01-19 07:57:26 +0200

    Delete chamber directory

--- chamber/scripts/getDates.js
+++
@@ -1,7 +0,0 @@
-let currentDateAndTime = new Date();
-let currentYear = currentDateAndTime.getFullYear();
-document.querySelector("#copyright").innerHTML = "&copy; " + currentYear;
-
-let lastModifiedFull = new Date(currentDateAndTime);
-let lastModified = `${lastModifiedFull.getMonth() + 1}/${lastModifiedFull.getDate()}/${currentYear} ${lastModifiedFull.getHours()}:${lastModifiedFull.getMinutes()}:${lastModifiedFull.getSeconds()}`;  
-document.getElementById("last-modified").innerHTML = "Last Modified: " + lastModified;
