220cc2e6610dc9d69b5160e6cc1231335f8b9d3b
lamediteko <dit21002@byui.edu>
2023-01-22 03:50:23 +0200

    week 3 update

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,4 @@
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+const date = `&copy; ${new Date().getFullYear()} Lame Diteko, Gaborone,Botswana`;
+document.getElementById('copyright').innerHTML = date;
